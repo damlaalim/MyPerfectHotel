8caac7d4f777e9cc2c2f5c92c58a29be8ecfeb98
damlaalim <damlaalim81@gmail.com>
2023-08-21 15:49:54 +0300

    - new model & idle and walk animations added
    - movement system updated

--- Assets/_MyPerfectHotel/Scripts/Player/PlayerMovement.cs
+++ Assets/_MyPerfectHotel/Scripts/Player/PlayerMovement.cs
@@ -6,19 +6,36 @@ namespace _MyPerfectHotel.Scripts.Player
     public class PlayerMovement : MonoBehaviour
     {
         [SerializeField] private float moveSpeed;
-        [SerializeField] private DynamicJoystick joystick;
-
+        [SerializeField] private DynamicJoystick joystick; 
+        
+        private Animator _animator;
         private Rigidbody _rigidbody;
+        private bool _isWalk;
         
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody>();
+            _animator = GetComponent<Animator>();
         }
 
         private void FixedUpdate()
         {
-            var dir = Vector3.forward * joystick.Vertical + Vector3.right * joystick.Horizontal;
-            _rigidbody.velocity = dir * moveSpeed * Time.fixedDeltaTime;
+            var dir = new Vector3(joystick.Horizontal * moveSpeed, _rigidbody.velocity.y, joystick.Vertical * moveSpeed);
+            _rigidbody.velocity = dir;
+            
+            if (joystick.Horizontal != 0 || joystick.Vertical != 0)
+                transform.rotation = Quaternion.LookRotation(_rigidbody.velocity);
+            
+            if (dir != Vector3.zero && !_isWalk)
+            {
+                _animator.CrossFade("Walking", .1f);
+                _isWalk = true;
+            }
+            else if (dir == Vector3.zero)
+            {
+                _animator.CrossFade("Idle", .1f);
+                _isWalk = false;
+            }
         }
     }
 }
